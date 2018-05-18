be70da89b52be144f4a3655f49ef9b9a07baf3b5
Dirk Ruschhaupt <dirkruschhaupt@Dirks-MacBook-Pro.local>
2018-05-18 15:33:44 -0500

    done

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-// Your JS goes here
+
+
+(function() {
+
+  function CheckerboardRow(rowNum) {
+    for(let i = 0; i < 9; i++) {
+      //console.log("affirmative");
+      let checkerboardSquare = document.createElement('div');
+
+      document.body.appendChild(checkerboardSquare);
+
+      //console.log(checkerboardSquare);
+      checkerboardSquare.style.width = "11.1%";
+      checkerboardSquare.style.float = "left";
+      checkerboardSquare.style.paddingBottom = "11.1%";
+      //if (i % 2 == rowNum % 2) {    //<===this line works also
+        if ((i + rowNum) % 2 === 0) {
+        checkerboardSquare.style.backgroundColor = 'black';
+      } else {
+        checkerboardSquare.style.backgroundColor = 'red';
+      };
+    }
+  }
+  for (let i = 0; i < 9; i++) {
+    CheckerboardRow(i);
+  };
+})();
