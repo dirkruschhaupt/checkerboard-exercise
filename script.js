f01fbdbfd3e8974f7c61772df90ab2aaed4e91ba
Dirk Ruschhaupt <36975328+dirkruschhaupt@users.noreply.github.com>
2018-05-21 09:41:22 -0500

    finished

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+//document.addEventListener('DOMContentLoader', function() {
 
 
 (function() {
+
+/*  const random = function() {
+    let num1 = Math.floor(Math.random() * 255);
+    let num2 = Math.floor(Math.random() * 255);
+    let num3 = Math.floor(Math.random() * 255);
+    return (`rgb(${numq}), (${num2}), (${num3})`);
+  }*/
 
   function CheckerboardRow(rowNum) {
     for(let i = 0; i < 9; i++) {
@@ -13,11 +21,12 @@
       checkerboardSquare.style.width = "11.1%";
       checkerboardSquare.style.float = "left";
       checkerboardSquare.style.paddingBottom = "11.1%";
+      //checkerboardSquare.style = "width = 11.1%; float = left; paddingBottom = 11.1%;";
       //if (i % 2 == rowNum % 2) {    //<===this line works also
-        if ((i + rowNum) % 2 === 0) {
-        checkerboardSquare.style.backgroundColor = 'black';
-      } else {
+      if ((i + rowNum) % 2 === 0) {
         checkerboardSquare.style.backgroundColor = 'red';
+      } else {
+        checkerboardSquare.style.backgroundColor = 'black';
       };
     }
   }
